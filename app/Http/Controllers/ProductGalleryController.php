ed4f9f0389d95fedbb25a4d063bb0bb84840f520
Syamil <syamil@codelabs.co.id>
2020-07-01 11:19:52 +0700

    delete gallery product

--- app/Http/Controllers/ProductGalleryController.php
+++ app/Http/Controllers/ProductGalleryController.php
@@ -91,6 +91,9 @@ class ProductGalleryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = ProductGallery::findOrFail($id);
+        $item->delete();
+
+        return redirect()->route('product-galleries.index');
     }
 }
